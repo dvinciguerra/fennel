cc1721b881b7141f75260184807f7adc22bc8491
Daniel Vinciguerra <daniel.vinciguerra@bivee.com.br>
2024-05-31 15:39:27 -0300

    chore: first commit

---
+++ spec/fennel_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fennel do
+  it "has a version number" do
+    expect(Fennel::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
